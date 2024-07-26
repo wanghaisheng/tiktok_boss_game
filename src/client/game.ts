97909b2e3418aafc29025fc5180ba688145ae189
Sølve Tornøe <solve@tornoee.com>
2024-07-27 00:00:08 +0200

    change height

--- src/client/game.ts
+++ src/client/game.ts
@@ -12,14 +12,14 @@ export class Game {
     constructor() {
         console.log('Game started')
 
-        this.app = new PIXI.Application({ width: 800, height: 600 })
+        this.app = new PIXI.Application({ width: 700, height: 1080 })
         this.app.renderer.backgroundColor = 0x1099bb
         document.body.appendChild(this.app.view)
 
         const monsters = [
             new Monster('Bunny', 2, 'https://pixijs.com/assets/bunny.png', this.app),
-            new Monster('Monster', 3, 'https://pixijs.com/assets/eggHead.png', this.app),
-            new Monster('Alien', 2500, 'https://pixijs.com/assets/flowerTop.png', this.app),
+            new Monster('Monster', 30, 'https://pixijs.com/assets/eggHead.png', this.app),
+            new Monster('Alien', 100, 'https://pixijs.com/assets/flowerTop.png', this.app),
         ]
 
         this.monsterManager = new MonsterManager(this.app, monsters)
